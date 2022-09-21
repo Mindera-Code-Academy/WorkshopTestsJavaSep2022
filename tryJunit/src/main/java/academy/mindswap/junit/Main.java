15cb38afc58e1dc4e91421cc7826c9a80fd52dc0
Rafael Miranda <rafael.miranda@mindera.com>
2022-09-21 13:02:34 +0100

    add gitignore

--- tryJunit/src/main/java/academy/mindswap/junit/Main.java
+++ tryJunit/src/main/java/academy/mindswap/junit/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator(new OtherClass());
         System.out.println(String.format("%d + %d = %d", 5, 7, calculator.sum(5, 7)));
-        System.out.println(String.format("Multiply even numbers result: %d", 
+        System.out.println(String.format("Multiply even numbers result: %d",
                 calculator.multiplyEvenNumbers(List.of(1, 2, 3, 4, 5, 6))));
         System.out.println(String.format("Random number - %s", calculator.getRandomReference()));
     }
